7652a7f78c744f12b5be41aaa4eca036b81c91eb
Vektor360 <87315210+Vektor360@users.noreply.github.com>
2024-05-18 20:19:45 +1100

    New

---
+++ vue-layers/export.bat
@@ -0,0 +1,7 @@
+echo "# Parsing-visual" >> README.md
+git init
+git add README.md
+git commit -m "first commit"
+git branch -M main
+git remote add origin git@github.com:Vektor360/Parsing-visual.git
+git push -u origin main
--- vue-layers/vue.config.js
+++ vue-layers/vue.config.js
@@ -1,4 +1,16 @@
 const { defineConfig } = require('@vue/cli-service')
 module.exports = defineConfig({
-  transpileDependencies: true
+  transpileDependencies: true,
+  devServer: {
+    port: 3000,
+    proxy: {
+      '/places': {
+        target: 'http://localhost:3000',
+        //changeOrigin: true,
+        pathRewrite: {
+          '^/places': '' // Если API сервера принимает запросы без /places, то уберите эту строку
+        }
+      }
+    }
+  }
 })
